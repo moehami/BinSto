2063a8d6c009a353d023c6db0778f1eb9e5206fe
moehami <m.aljaaly@yahoo.co.uk>
2024-09-30 01:42:34 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Colorado from '../components/StatesList'
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Head>
+        <title>US Bin Stores</title>
+        <meta name="description" content="Find bin stores in US" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <StatesList />
+      </main>
+
+      <footer className="text-center py-4">
+        <p>&copy; 2024 US Bin Stores Directory</p>
+      </footer>
+    </div>
+  )
+}
