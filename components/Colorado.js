9bbd6f6b16079733b81c1e52b81db44e1e61333c
moehami <m.aljaaly@yahoo.co.uk>
2024-10-17 12:57:35 +0300

    Update Colorado.js

--- components/Colorado.js
+++ components/Colorado.js
@@ -30,10 +30,7 @@ const StoreCard = React.memo(({ store }) > (
             <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> {store.phone_number}</dd>
           </div>
 
-              
-
-
-      </div>
+            
     </div> ));
 
 
